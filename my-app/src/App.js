2607e2005b9daa01fde180a7a38bcfa79f66c3c4
Astha <asthakarn216@gmail.com>
2023-04-24 15:49:33 +0530

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import ApiCall from './components/ApiCall';
+// import TagInput from './components/TagInput';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ApiCall/>
+      {/* <TagInput/> */}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/ApiCall.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./ApiCallStyles.css"
+let limit = 6;
+
+const ApiCall = () => {
+    const [posts, setPosts] = useState([])
+    const [visibleItems, setVisibleItems] = useState(limit);
+
+
+    // console.log("visbleitem", visibleItems)
+
+    const handleLoadMore = () => {
+        setVisibleItems(visibleItems + limit);
+    }
+    useEffect(() => {
+        axios
+            .get('https://dummyjson.com/posts')
+
+            .then((res) => {
+                console.log(res)
+                setPosts(res.data.posts)
+            }).catch((err) => {
+                console.log('error')
+            })
+    }, [])
+
+
+    const handleTagChange = (event) => {
+        const newTags = event.target.value.split(/[, ]+/);
+        setPosts(newTags)
+      };
+
+    console.log("postssssss", posts)
+    return (
+
+        <div className="work-container">
+            <h1 className="project-heading">DETRATOR ASSIGNMENT</h1>
+            <div className='parent-container'>
+                {posts?.slice(0, visibleItems)?.map((item, index) => (
+                    <div className='grid-item' key={index}>
+                        <div className='title'><h2>{item.title}</h2></div>
+                        <div className='body'>{item.body}</div>
+                        <div className="about">
+                            <div onChange={handleTagChange}>Tags : {item.tags.join()}</div>
+                            <div>Reaction : {item.reactions}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className='btn'>
+                    {visibleItems < posts.length && (
+                        <button className='' onClick={handleLoadMore}>View More</button>
+                    )}
+                </div>
+        </div>
+    )
+}
+
+export default ApiCall
